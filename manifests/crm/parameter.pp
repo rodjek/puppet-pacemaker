f95646ee8c93874d535880f132b9430a4b4699b6
Tim Sharpe <tim@sharpe.id.au>
2010-03-24 11:35:38 +1100

    We only want to explicitly cleanup resources

--- manifests/crm/parameter.pp
+++ manifests/crm/parameter.pp
@@ -1,9 +1,5 @@
 define ha::crm::parameter($resource, $parameter, $value, $ensure=present, $ignore_dc="false") {
     if($ha_cluster_dc == $fqdn) or ($ignore_dc == "true") {
-        ha::resetstate { "${resource}-${parameter}":
-            resource => $resource,
-        }
-
         if($ensure == absent) {
             exec { "Removing ${parameter} from ${resource}":
                 command => "/usr/sbin/crm_resource -r ${resource} -d ${parameter}",
@@ -13,7 +9,6 @@ define ha::crm::parameter($resource, $parameter, $value, $ensure=present, $ignor
             exec { "Setting ${parameter} on ${resource} to ${value}":
                 command => "/usr/sbin/crm_resource -r ${resource} -p ${parameter} -v \"${value}\"",
                 unless  => "/usr/bin/test `/usr/sbin/crm_resource -r ${resource} -g ${parameter}` = \"${value}\"",
-                notify  => Ha::Resetstate["${resource}-${parameter}"],
             }
         }
     }
