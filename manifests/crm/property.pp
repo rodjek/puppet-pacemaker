c052ee0f20d53102f06402ad3cdd7f188e907fbb
Tim Sharpe <tim@sharpe.id.au>
2010-03-24 11:44:15 +1100

    It'd be nice to be able delete properties

--- manifests/crm/property.pp
+++ manifests/crm/property.pp
@@ -1,8 +1,15 @@
-define ha::crm::property($value) {
+define ha::crm::property($value, $ensure=present) {
     if($ha_cluster_dc == $fqdn) {
-        exec { "Setting CRM property ${name} to ${value}":
-            command => "/usr/sbin/crm_attribute -n ${name} -v ${value}",
-            unless  => "/usr/bin/test `/usr/sbin/crm_attribute -t crm_config -n ${name} -G -Q` = \"${value}\"",
+        if($ensure == absent) {
+            exec { "Deleting CRM property ${name}":
+                command => "/usr/sbin/crm_attribute -t crm_config -n ${name} -D",
+                onlyif  => "/usr/sbin/crm_attribute -t crm_config -n ${name} -G -Q",
+            }
+        } else {
+            exec { "Setting CRM property ${name} to ${value}":
+                command => "/usr/sbin/crm_attribute -n ${name} -v ${value}",
+                unless  => "/usr/bin/test `/usr/sbin/crm_attribute -t crm_config -n ${name} -G -Q` = \"${value}\"",
+            }
         }
     }
 }
