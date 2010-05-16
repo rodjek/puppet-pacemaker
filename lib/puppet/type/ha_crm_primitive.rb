62e41f4d4be921fb40c153ca234c66089fb79f30
Tim Sharpe <tim@sharpe.id.au>
2010-05-17 09:53:35 +1000

    Tab damage

--- lib/puppet/type/ha_crm_primitive.rb
+++ lib/puppet/type/ha_crm_primitive.rb
@@ -1,101 +1,99 @@
 Puppet::Type.newtype(:ha_crm_primitive) do
-    # TODO
-    #
-    # * Set the default ensure value to true
-    # * The ablity to run the resources only on the DC
+	# TODO
+	#
+	# * Set the default ensure value to true
 
-    @doc = "Manage Pacemaker primitives"
+	@doc = "Manage Pacemaker primitives"
 
-    ensurable
+	ensurable
 
-    newparam(:type) do
-        desc "The name of the Resource Agent you with to use. eg. IPAddr or Filesystem"
-    end
+	newparam(:type) do
+		desc "The name of the Resource Agent you with to use. eg. IPAddr or Filesystem"
+	end
 
-    newparam(:id) do
-        desc "The name of the resource"
+	newparam(:id) do
+		desc "The name of the resource"
 
-        isnamevar
-    end
+		isnamevar
+	end
 
-    newparam(:only_run_on_dc, :boolean => true) do
-        desc "In order to prevent race conditions, we generally only want to
-              make changes to the CIB on a single machine (in this case, the
-              Designated Controller)."
+	newparam(:only_run_on_dc, :boolean => true) do
+		desc "In order to prevent race conditions, we generally only want to
+					make changes to the CIB on a single machine (in this case, the
+					Designated Controller)."
 
-        newvalues(:true, :false)
-        defaultto(:true)
-    end
+		newvalues(:true, :false)
+		defaultto(:true)
+	end
 
-    newproperty(:priority) do
-        desc "The priority of the resource"
+	newproperty(:priority) do
+		desc "The priority of the resource"
 
-        newvalues(:absent, /\d+/)
-        defaultto :absent
-    end
+		newvalues(:absent, /\d+/)
+		defaultto :absent
+	end
 
-    newproperty(:target_role) do
-        desc "What state should the cluster attempt to keep this resource in?
+	newproperty(:target_role) do
+		desc "What state should the cluster attempt to keep this resource in?
         
-              Allowed values:
-                * stopped - Force the resource not to run
-                * started - Allow the resource to be started
-                * master -  Allow the resource to be started and promoted to Master"
+					Allowed values:
+						* stopped - Force the resource not to run
+						* started - Allow the resource to be started
+						* master -  Allow the resource to be started and promoted to Master"
 
-        newvalues(:absent, :stopped, :started, :master)
-        defaultto :absent
-    end
+		newvalues(:absent, :stopped, :started, :master)
+		defaultto :absent
+	end
 
-    newproperty(:is_managed) do
-        desc "Is the cluster allowed to start and stop the resource?"
+	newproperty(:is_managed) do
+		desc "Is the cluster allowed to start and stop the resource?"
 
-        newvalues(:absent, :true, :false)
-        defaultto :absent
-    end
+		newvalues(:absent, :true, :false)
+		defaultto :absent
+	end
 
-    newproperty(:resource_stickiness) do
-        desc "How much does the resource prefer to stay where it is?
+	newproperty(:resource_stickiness) do
+		desc "How much does the resource prefer to stay where it is?
 
-              This defaults to 'inherited', which is the value of 
-              resource-stickiness in the rsc_defaults section"
+					This defaults to 'inherited', which is the value of 
+					resource-stickiness in the rsc_defaults section"
 
-        newvalues(:absent, /\d+/)
-        defaultto :absent
-    end
+		newvalues(:absent, /\d+/)
+		defaultto :absent
+	end
 
-    newproperty(:migration_threshold) do
-        desc "How many failures should occur for this resource on a node
-              before making the node ineligible to host this resource."
+	newproperty(:migration_threshold) do
+		desc "How many failures should occur for this resource on a node
+					before making the node ineligible to host this resource."
 
-        newvalues(:absent, /\d+/)
-        defaultto :absent
-    end
+		newvalues(:absent, /\d+/)
+		defaultto :absent
+	end
 
-    newproperty(:failure_timeout) do
-        desc "How many seconds to wait before acting as if the failure had
-              not occurred (and potentially allowing the resource back to the
-              node on which it failed."
+	newproperty(:failure_timeout) do
+		desc "How many seconds to wait before acting as if the failure had
+					not occurred (and potentially allowing the resource back to the
+					node on which it failed."
     
-        newvalues(:absent, /\d+/)
-        defaultto :absent
-    end
+		newvalues(:absent, /\d+/)
+		defaultto :absent
+	end
 
-    newproperty(:multiple_active) do
-        desc "What should the cluster do if it ever finds the resource active
-              on more than one node.
+	newproperty(:multiple_active) do
+		desc "What should the cluster do if it ever finds the resource active
+					on more than one node.
 
-              Allowed values:
-                * block      - Mark the resource as unmanaged
-                * stop_only  - Stop all active instances and leave them that way
-                * stop_start - Stop all active instances and start the resource
-                               in one location only."
+					Allowed values:
+						* block      - Mark the resource as unmanaged
+						* stop_only  - Stop all active instances and leave them that way
+						* stop_start - Stop all active instances and start the resource
+													 in one location only."
     
-        newvalues(:absent, :block, :stop_only, :stop_start)
-        defaultto :absent
-    end
+		newvalues(:absent, :block, :stop_only, :stop_start)
+		defaultto :absent
+	end
 
-    validate do
-        raise Puppet::Error, "You must specify a class for this primitive" unless @parameters.include?(:class)
-        raise Puppet::Error, "You must specify a type for this primitive" unless @parameters.include?(:type)
-    end
+	validate do
+		raise Puppet::Error, "You must specify a type for this primitive" unless @parameters.include?(:type)
+	end
 end
