8df9431260955c963314290fffe6df036532513d
Tim Sharpe <tim@sharpe.id.au>
2010-05-16 16:36:03 +1000

    Validate for required parameters

--- lib/puppet/type/ha_crm_primitive.rb
+++ lib/puppet/type/ha_crm_primitive.rb
@@ -18,4 +18,9 @@ Puppet::Type.newtype(:ha_crm_primitive) do
 
         isnamevar
     end
+
+    validate do
+        raise Puppet::Error, "You must specify a class for this primitive" unless @parameters.include?(:class)
+        raise Puppet::Error, "You must specify a type for this primitive" unless @parameters.include?(:type)
+    end
 end
