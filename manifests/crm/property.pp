485f10c6dad749f3df05dc1387f1d67aae53e758
Tim Sharpe <tim@sharpe.id.au>
2011-04-30 09:21:38 +0200

    Allow any property type

--- manifests/crm/property.pp
+++ manifests/crm/property.pp
@@ -1,14 +1,18 @@
-define ha::crm::property($value, $ensure=present) {
+define ha::crm::property($value, $type='crm_config', $ensure=present) {
+    if($type !~ /(crm_config|rsc_defaults|op_defaults)/) {
+        fail("Unknown property type '${type}' passed to ha::crm::property")
+    }
+
     if($ha_cluster_dc == $fqdn) {
         if($ensure == absent) {
             exec { "Deleting CRM property ${name}":
-                command => "/usr/sbin/crm_attribute -t crm_config -n ${name} -D",
-                onlyif  => "/usr/sbin/crm_attribute -t crm_config -n ${name} -G -Q",
+                command => "/usr/sbin/crm_attribute -t ${type} -n ${name} -D",
+                onlyif  => "/usr/sbin/crm_attribute -t ${type} -n ${name} -G -Q",
             }
         } else {
             exec { "Setting CRM property ${name} to ${value}":
-                command => "/usr/sbin/crm_attribute -n ${name} -v ${value}",
-                unless  => "/usr/bin/test `/usr/sbin/crm_attribute -t crm_config -n ${name} -G -Q` = \"${value}\"",
+                command => "/usr/sbin/crm_attribute -t ${type} -n ${name} -v ${value}",
+                unless  => "/usr/bin/test `/usr/sbin/crm_attribute -t ${type} -n ${name} -G -Q` = \"${value}\"",
             }
         }
     }
