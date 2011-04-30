ddde21949c08afecaca188c01b91c49f760b0f52
Tim Sharpe <tim@sharpe.id.au>
2011-04-30 09:27:03 +0200

    Documentation for ha::crm::property

--- manifests/crm/property.pp
+++ manifests/crm/property.pp
@@ -1,3 +1,24 @@
+# Public: Manage cluster properties/attributes.
+# 
+# namevar - The String name of the property.
+# value   - The String value of the property.
+# type    - The String type of the property to be set.  Valid values are
+#           crm_config, rsc_defaults and op_defaults (default: crm_config).
+# ensure  - The String desired state of the property.  Valid values are
+#           present and absent (default: present).
+#
+# Examples
+#
+#   # Enable STONITH on a cluster
+#   ha::crm::property { 'stonith-enabled':
+#     value => 'true',
+#   }
+#
+#   # Set a default stickiness value for all resources in inherit
+#   ha::crm::property { 'resource-stickiness':
+#     value => '100',
+#     type  => 'rsc_defaults',
+#   }
 define ha::crm::property($value, $type='crm_config', $ensure=present) {
     if($type !~ /(crm_config|rsc_defaults|op_defaults)/) {
         fail("Unknown property type '${type}' passed to ha::crm::property")
